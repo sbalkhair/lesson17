30336824f93d958648afaa03518433f04fc81124
saeed <s.balkhair@hotmail.com>
2020-11-21 10:22:52 +0300

    Initial check-in of module lesson17

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace lesson17
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push("saeed");
+            stack.Push("Roya");
+            stack.Push("Nonoy");
+            stack.Pop();
+            Console.WriteLine(stack.Size());
+            stack.Print();
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lesson17
+{
+    public class Stack
+    {
+
+        List<string> mylist;
+        public Stack()
+        {
+            mylist = new List<string>();
+        }
+
+
+        public void Push(string item)
+        {
+            mylist.Add(item);
+        }
+
+        public void Pop()
+        {
+            mylist.Remove(mylist[mylist.Count - 1]);
+        }
+
+        public int Size()
+        {
+            return mylist.Count;
+        }
+
+        public void Print()
+        {
+            foreach (var item in mylist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
